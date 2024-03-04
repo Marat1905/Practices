de8b33368f876dff07c9d7fad712c99aa722dcac
Marat <milya010790@mail.ru>
2024-03-04 18:14:27 +0500

    Добавьте файлы проекта.

---
+++ Practices.Calc/Calculator.cs
@@ -0,0 +1,42 @@
+﻿namespace Practices.Calc
+{
+    /// <summary>Кастомный калькулятор </summary>
+    public class Calculator
+    {
+        /// <summary>Сумма двух аргументов.</summary>
+        /// <param name="a">Первый аргумент</param>
+        /// <param name="b">Второй аргумент</param>
+        /// <returns>Возвращаем результат</returns>
+        public int Additional(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>Разность двух аргументов.</summary>
+        /// <param name="a">Первый аргумент</param>
+        /// <param name="b">Второй аргумент</param>
+        /// <returns>Возвращаем результат</returns>
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>Произведение двух аргументов.</summary>
+        /// <param name="a">Первый аргумент</param>
+        /// <param name="b">Второй аргумент</param>
+        /// <returns>Возвращаем результат</returns>
+        public int Miltiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>Частное двух аргументов.</summary>
+        /// <param name="a">Первый аргумент</param>
+        /// <param name="b">Второй аргумент</param>
+        /// <returns>Возвращаем результат</returns>
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Practices.Tests/CalculatorTests.cs
@@ -0,0 +1,36 @@
+﻿using Practices.Calc;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustAlwaysReturnsExpectedValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Additional(10, 220), Is.EqualTo(230));
+        }
+
+        [Test]
+        public void SubtractionMustAlwaysReturnsExpectedValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Subtraction(100, 50), Is.EqualTo(50));
+        }
+
+        [Test]
+        public void MiltiplicationMustAlwaysReturnsExpectedValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Miltiplication(100, 50), Is.EqualTo(5000));
+        }
+
+        [Test]
+        public void DivisionMustAlwaysReturnsExpectedValue()
+        {
+            var calculatorTest = new Calculator();
+            Assert.That(calculatorTest.Division(100, 50), Is.EqualTo(2));
+        }
+    }
+}
